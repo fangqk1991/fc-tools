4504f97b83ab8c7315b5a27d1854b7ae6b67f3e1
fangqk1991 <me@fangqk.com>
2019-07-15 21:00:02 +0800

    update test cases

--- tests/js-tests/Signature.test.js
+++ tests/js-tests/Signature.test.js
@@ -3,14 +3,17 @@ const { Signature } = require('../../lib')
 
 describe('Test MySignature', () => {
   it(`Normal Test`, async () => {
+    const appid = 'abcdefg'
     const secret = '123456'
     const signature = new Signature(secret, 600)
     const expires = signature.getExpires()
     const queryParams = {
       x: 123,
       y: 456,
-      _expires: expires,
     }
+    queryParams['_appid'] = appid
+    queryParams['_expires'] = expires
+
     const method = 'GET'
     const apiPath = '/api/xxx/'
     const body = {
--- tests/ts-tests/Signature.test.ts
+++ tests/ts-tests/Signature.test.ts
@@ -3,14 +3,17 @@ import { Signature } from '../../src'
 
 describe('Test MySignature', () => {
   it(`Normal Test`, async () => {
+    const appid = 'abcdefg'
     const secret = '123456'
     const signature = new Signature(secret, 600)
     const expires = signature.getExpires()
     const queryParams: any = {
       x: 123,
       y: 456,
-      _expires: expires,
     }
+    queryParams['_appid'] = appid
+    queryParams['_expires'] = expires
+
     const method = 'GET'
     const apiPath = '/api/xxx/'
     const body = {
